962b10f2dce3fc9f1eacd0a961420480c56c62fb
devcihat <cihattsungu@outlook.com>
2020-12-27 20:03:36 +0300

    1 file modified

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -43,6 +43,82 @@ function About(props) {
             <small className="text-muted">Donec id elit non mi porta.</small>
           </a>
         </div>
+        <div className="list-group">
+          <a
+            className="list-group-item list-group-item-action mb-3 "
+            aria-current="true"
+          >
+            <div className="d-flex w-100 justify-content-between">
+              <h5 className="mb-1">List group item heading</h5>
+              <small>3 days ago</small>
+            </div>
+            <p className="mb-1">
+              Donec id elit non mi porta gravida at eget metus. Maecenas sed
+              diam eget risus varius blandit.
+            </p>
+            <small>Donec id elit non mi porta.</small>
+          </a>
+          <a className="list-group-item list-group-item-action mb-3">
+            <div className="d-flex w-100 justify-content-between">
+              <h5 className="mb-1">List group item heading</h5>
+              <small className="text-muted">3 days ago</small>
+            </div>
+            <p className="mb-1">
+              Donec id elit non mi porta gravida at eget metus. Maecenas sed
+              diam eget risus varius blandit.
+            </p>
+            <small className="text-muted">Donec id elit non mi porta.</small>
+          </a>
+          <a className="list-group-item list-group-item-action">
+            <div className="d-flex w-100 justify-content-between">
+              <h5 className="mb-1">List group item heading</h5>
+              <small className="text-muted">3 days ago</small>
+            </div>
+            <p className="mb-1">
+              Donec id elit non mi porta gravida at eget metus. Maecenas sed
+              diam eget risus varius blandit.
+            </p>
+            <small className="text-muted">Donec id elit non mi porta.</small>
+          </a>
+        </div>
+        <div className="list-group">
+          <a
+            className="list-group-item list-group-item-action mb-3 "
+            aria-current="true"
+          >
+            <div className="d-flex w-100 justify-content-between">
+              <h5 className="mb-1">List group item heading</h5>
+              <small>3 days ago</small>
+            </div>
+            <p className="mb-1">
+              Donec id elit non mi porta gravida at eget metus. Maecenas sed
+              diam eget risus varius blandit.
+            </p>
+            <small>Donec id elit non mi porta.</small>
+          </a>
+          <a className="list-group-item list-group-item-action mb-3">
+            <div className="d-flex w-100 justify-content-between">
+              <h5 className="mb-1">List group item heading</h5>
+              <small className="text-muted">3 days ago</small>
+            </div>
+            <p className="mb-1">
+              Donec id elit non mi porta gravida at eget metus. Maecenas sed
+              diam eget risus varius blandit.
+            </p>
+            <small className="text-muted">Donec id elit non mi porta.</small>
+          </a>
+          <a className="list-group-item list-group-item-action">
+            <div className="d-flex w-100 justify-content-between">
+              <h5 className="mb-1">List group item heading</h5>
+              <small className="text-muted">3 days ago</small>
+            </div>
+            <p className="mb-1">
+              Donec id elit non mi porta gravida at eget metus. Maecenas sed
+              diam eget risus varius blandit.
+            </p>
+            <small className="text-muted">Donec id elit non mi porta.</small>
+          </a>
+        </div>
       </div>
     </div>
   );
