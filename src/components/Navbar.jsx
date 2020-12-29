b1494b53b3b45e170df38172556f30432784274e
devcihat <cihattsungu@outlook.com>
2020-12-29 20:13:48 +0300

    2 files modified, 1 added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,7 +39,7 @@ function Navbar() {
             </li>
             <li className="nav-item ">
               <Link to="/card" className="nav-link text-white">
-                <i>Card</i>
+                <i>Post</i>
               </Link>
             </li>
           </ul>
